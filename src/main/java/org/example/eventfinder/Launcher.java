ebcfa57de662abbc4d7e2c6b3cc26cb17bc8cf01
jade <j.albrecht@edu.letteverein.berlin>
2025-12-01 11:26:03 +0100

    More Setup

--- src/main/java/org/example/eventfinder/Launcher.java
+++ src/main/java/org/example/eventfinder/Launcher.java
@@ -2,6 +2,7 @@ package org.example.eventfinder;
 
 import javafx.application.Application;
 public class Launcher {
+    //Comment for change
     public static void main(String[] args){
         Application.launch(HelloApplication.class,args);
     }
