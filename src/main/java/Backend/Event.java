7a9a2f1c9c95c3774444301437b3cca12125a415
jade <j.albrecht@edu.letteverein.berlin>
2025-12-08 11:13:28 +0100

    Update Commit

--- src/main/java/Backend/Event.java
+++ src/main/java/Backend/Event.java
@@ -1,4 +1,31 @@
 package Backend;
 
+import com.google.gson.JsonElement;
+
 public class Event {
+    String location;
+    String date;
+    String dayANDtime;
+    String venue;
+
+    public Event(String location, JsonElement date, String venue) {
+        this.location = location;
+        this.date = JsonTools.getValue(date,SearchID.START_DATE).toString();
+        this.dayANDtime=JsonTools.getValue(date,SearchID.WHEN).toString();
+        this.venue = venue;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDayANDtime(){return dayANDtime;}
+
+    public String getVenue() {
+        return venue;
+    }
 }
